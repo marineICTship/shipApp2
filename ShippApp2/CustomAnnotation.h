c03c532eadb877b46217a356b4eeeecef4f92608
TakahiroTsujiura <b1012178@fun.ac.jp>
2014-11-15 12:51:56 +0900

    停止アイコンで船舶を表示

---
+++ ShippApp2/CustomAnnotation.h
@@ -0,0 +1,31 @@
+//
+//  CustomAnnotation.h
+//  ShippApp2
+//
+//  Created by codepro on 2014/11/15.
+//  Copyright (c) 2014年 FutureUniversityHakodate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+
+@interface CustomAnnotation : NSObject <MKAnnotation> {
+    CLLocationCoordinate2D coordinate;
+    NSString *title;
+    NSString *subtitle;
+    //MKPinAnnotationColor pinColor;
+    
+    UIImage *img;
+}
+
+@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic)MKPinAnnotationColor pinColor;
+@property (nonatomic)UIImage *shipimg;
+@property (nonatomic)UIImage *img;
+
+- (id)initWithCoordinates:(CLLocationCoordinate2D)co newTitle:(NSString *)t newSubTitle:(NSString *)st newimg:(UIImage *)img;
+
+@end
+
